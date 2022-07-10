e14c1a861320ed567915386434543d02320e822a
sadssssaaa <109033022+sadssssaaa@users.noreply.github.com>
2022-07-10 18:54:17 +0300

    Add files via upload

---
+++ alan.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+int main()
+{const double pi=3.14159;
+int r,cevre,alan;
+
+cout<<"yarýcapi giriniz";
+cin>>r;
+
+
+cevre=2*pi*r;
+alan=pi*r*r;
+
+
+
+cout<<"cevre= "<<cevre ;
+cout<<"alan= "<<alan;
+
+return 0;
+}
